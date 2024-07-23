7b58ba0536226dda7ab48e5cc29ae7d2adb5982c
ofmi <goncarm354@gmail.com>
2024-07-23 18:25:27 +0300

    Added some new features and testing function

--- stack_allocator/LinkedList.h
+++ stack_allocator/LinkedList.h
@@ -1,7 +1,8 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-#include "AllocatorTraits.h"
+#include <memory>
+
 #include "StackAllocator.h"
 
 template <typename T, typename Alloc = StackAllocator<T>>
@@ -18,22 +19,67 @@ class LinkedList {
   using pointer = typename std::allocator_traits<allocator_type>::pointer;
   using size_type = typename std::allocator_traits<allocator_type>::size_type;
 
-  LinkedList() : head_(nullptr) {}
+  LinkedList() : head_(nullptr), allocator_() {}
 
+  LinkedList(const std::initializer_list<T>& list)
+      : head_(nullptr), allocator_() {
+    for (auto it = list.begin(); it != list.end(); ++it) {
+      push_back(*it);
+    }
+  }
+
+  LinkedList(const LinkedList& other)
+      : head_(nullptr),
+        allocator_(
+            std::allocator_traits<allocator_type>::
+                select_on_container_copy_construction(other.get_allocator())) {
+    for (auto it = other.begin(); it != other.end(); ++it) {
+      push_back(*it);
+    }
+  }
   ~LinkedList() { clear(); }
+
+  LinkedList& operator=(const LinkedList& other) {
+    if (this != &other) {
+      clear();
+      if (std::allocator_traits<
+              allocator_type>::propagate_on_container_copy_assignment::value) {
+        allocator_ = other.allocator_;
+      }
+      for (auto it = other.begin(); it != other.end(); ++it) {
+        push_back(*it);
+      }
+    }
+    return *this;
+  }
 
   void push_front(const T& value) {
     pointer new_node = allocator_.allocate(1);
-    AllocatorTraits<allocator_type>::construct(allocator_, new_node,
-                                               Node{value, head_});
+    std::allocator_traits<allocator_type>::construct(allocator_, new_node,
+                                                     Node{value, head_});
     head_ = new_node;
+  }
+
+  void push_back(const T& value) {
+    pointer new_node = allocator_.allocate(1);
+    std::allocator_traits<allocator_type>::construct(allocator_, new_node,
+                                                     Node{value, nullptr});
+    if (head_ == nullptr) {
+      head_ = new_node;
+    } else {
+      Node* temp = head_;
+      while (temp->next) {
+        temp = temp->next;
+      }
+      temp->next = new_node;
+    }
   }
 
   void pop_front() {
     if (head_) {
       pointer temp = head_;
       head_ = head_->next;
-      AllocatorTraits<allocator_type>::destroy(allocator_, temp);
+      std::allocator_traits<allocator_type>::destroy(allocator_, temp);
       allocator_.deallocate(temp, 1);
     }
   }
@@ -43,6 +89,8 @@ class LinkedList {
       pop_front();
     }
   }
+
+  allocator_type get_allocator() const noexcept { return allocator_; }
 
   // Iterator for LinkedList
   class iterator {
@@ -60,12 +108,15 @@ class LinkedList {
       return node_ != other.node_;
     }
 
+    bool operator==(const iterator& other) const {
+      return node_ == other.node_;
+    }
+
    private:
     Node* node_;
   };
 
   iterator begin() const { return iterator(head_); }
-
   iterator end() const { return iterator(nullptr); }
 
  private:
--- stack_allocator/main.cpp
+++ stack_allocator/main.cpp
@@ -1,6 +1,8 @@
+#include <cassert>
 #include <chrono>
 #include <forward_list>
 #include <iostream>
+#include <memory>
 
 #include "LinkedList.h"
 #include "StackAllocator.h"
@@ -27,6 +29,7 @@ void test_stack_allocator() {
   for (int i = 0; i < 5; ++i) {
     std::cout << &(p1[i]) << " ";
   }
+  std::cout << '\n';
 
   // Destroy and deallocate memory
   for (int i = 0; i < 5; ++i) {
@@ -37,10 +40,7 @@ void test_stack_allocator() {
 
 // Test function for LinkedList with StackAllocator
 void test_linked_list() {
-  LinkedList<int> list;
-  list.push_front(1);
-  list.push_front(2);
-  list.push_front(3);
+  LinkedList<int> list{1, 2, 3};
 
   // Print the linked list
   std::cout << "Linked list after pushing: ";
@@ -77,6 +77,277 @@ void benchmark_list(ListType& list, int num_elements) {
   std::cout << "Time taken: " << duration.count() << " seconds" << '\n';
 }
 
+template <typename ListType1, typename ListType2>
+bool compare_lists(const ListType1& list1, const ListType2& list2) {
+  auto it1 = list1.begin();
+  auto it2 = list2.begin();
+
+  while (it1 != list1.end() && it2 != list2.end()) {
+    if (*it1 != *it2) {
+      return false;
+    }
+    ++it1;
+    ++it2;
+  }
+  return (it1 == list1.end() && it2 == list2.end());
+}
+
+void test_copy_constructor_and_assignment_operator() {
+  std::cout << "\nTesting copy constructor and assignment operator..." << '\n';
+
+  // Default allocator
+  LinkedList<int> list1{1, 2, 3};
+  LinkedList<int> list2 = list1;  // Copy constructor
+
+  std::cout << "list1 (default allocator): ";
+  for (auto elem : list1) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+
+  std::cout << "list2 (copied from list1, default allocator): ";
+  for (auto elem : list2) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+
+  LinkedList<int> list3;
+  list3 = list1;  // Assignment operator
+
+  std::cout << "list3 (assigned from list1, default allocator): ";
+  for (auto elem : list3) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+
+  // Custom allocator
+  LinkedList<int, StackAllocator<int>> list4{4, 5, 6};
+  LinkedList<int, StackAllocator<int>> list5 = list4;  // Copy constructor
+
+  std::cout << "list4 (custom allocator): ";
+  for (auto elem : list4) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+
+  std::cout << "list5 (copied from list4, custom allocator): ";
+  for (auto elem : list5) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+
+  LinkedList<int, StackAllocator<int>> list6;
+  list6 = list4;  // Assignment operator
+
+  std::cout << "list6 (assigned from list4, custom allocator): ";
+  for (auto elem : list6) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+}
+
+void test_compare_with_std_forward_list() {
+  std::cout << "\nTesting LinkedList against std::forward_list..." << '\n';
+
+  LinkedList<int> custom_list{1, 2, 3};
+  std::forward_list<int> std_list{1, 2, 3};
+
+  // Compare initial state
+  if (!compare_lists(custom_list, std_list)) {
+    std::cout << "Initial lists do not match!\n";
+    std::cout << "Custom list: ";
+    for (auto elem : custom_list) {
+      std::cout << elem << " ";
+    }
+    std::cout << '\n';
+
+    std::cout << "std::forward_list: ";
+    for (auto elem : std_list) {
+      std::cout << elem << " ";
+    }
+    std::cout << '\n';
+  }
+  assert(compare_lists(custom_list, std_list));
+  std::cout << "Initial lists are equal.\n";
+
+  // Push elements
+  custom_list.push_front(4);
+  std_list.push_front(4);
+  if (!compare_lists(custom_list, std_list)) {
+    std::cout << "After push_front(4), lists do not match!\n";
+    std::cout << "Custom list: ";
+    for (auto elem : custom_list) {
+      std::cout << elem << " ";
+    }
+    std::cout << '\n';
+
+    std::cout << "std::forward_list: ";
+    for (auto elem : std_list) {
+      std::cout << elem << " ";
+    }
+    std::cout << '\n';
+  }
+  assert(compare_lists(custom_list, std_list));
+  std::cout << "After push_front(4), lists are equal.\n";
+
+  custom_list.push_front(5);
+  std_list.push_front(5);
+  if (!compare_lists(custom_list, std_list)) {
+    std::cout << "After push_front(5), lists do not match!\n";
+    std::cout << "Custom list: ";
+    for (auto elem : custom_list) {
+      std::cout << elem << " ";
+    }
+    std::cout << '\n';
+
+    std::cout << "std::forward_list: ";
+    for (auto elem : std_list) {
+      std::cout << elem << " ";
+    }
+    std::cout << '\n';
+  }
+  assert(compare_lists(custom_list, std_list));
+  std::cout << "After push_front(5), lists are equal.\n";
+
+  // Pop elements
+  custom_list.pop_front();
+  std_list.pop_front();
+  if (!compare_lists(custom_list, std_list)) {
+    std::cout << "After pop_front(), lists do not match!\n";
+    std::cout << "Custom list: ";
+    for (auto elem : custom_list) {
+      std::cout << elem << " ";
+    }
+    std::cout << '\n';
+
+    std::cout << "std::forward_list: ";
+    for (auto elem : std_list) {
+      std::cout << elem << " ";
+    }
+    std::cout << '\n';
+  }
+  assert(compare_lists(custom_list, std_list));
+  std::cout << "After pop_front(), lists are equal.\n";
+
+  custom_list.pop_front();
+  std_list.pop_front();
+  if (!compare_lists(custom_list, std_list)) {
+    std::cout << "After another pop_front(), lists do not match!\n";
+    std::cout << "Custom list: ";
+    for (auto elem : custom_list) {
+      std::cout << elem << " ";
+    }
+    std::cout << '\n';
+
+    std::cout << "std::forward_list: ";
+    for (auto elem : std_list) {
+      std::cout << elem << " ";
+    }
+    std::cout << '\n';
+  }
+  assert(compare_lists(custom_list, std_list));
+  std::cout << "After another pop_front(), lists are equal.\n";
+
+  // Clear lists
+  custom_list.clear();
+  std_list.clear();
+  if (!compare_lists(custom_list, std_list)) {
+    std::cout << "After clear(), lists do not match!\n";
+    std::cout << "Custom list: ";
+    for (auto elem : custom_list) {
+      std::cout << elem << " ";
+    }
+    std::cout << '\n';
+
+    std::cout << "std::forward_list: ";
+    for (auto elem : std_list) {
+      std::cout << elem << " ";
+    }
+    std::cout << '\n';
+  }
+  assert(compare_lists(custom_list, std_list));
+  std::cout << "After clear(), lists are equal.\n";
+}
+
+void test_copy_constructor_and_assignment() {
+  // Test std::forward_list
+  std::forward_list<int, std::allocator<int>> std_list1{1, 2, 3};
+  std::forward_list<int, std::allocator<int>> std_list2(
+      std_list1);  // Copy constructor
+  std::forward_list<int, std::allocator<int>> std_list3;
+  std_list3 = std_list1;  // Assignment operator
+
+  // Test LinkedList with default allocator
+  LinkedList<int> linked_list1{1, 2, 3};
+  LinkedList<int> linked_list2(linked_list1);  // Copy constructor
+  LinkedList<int> linked_list3;
+  linked_list3 = linked_list1;  // Assignment operator
+
+  // Print lists
+  std::cout << "std::forward_list 1: ";
+  for (auto elem : std_list1) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+
+  std::cout << "std::forward_list 2 (copy): ";
+  for (auto elem : std_list2) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+
+  std::cout << "std::forward_list 3 (assignment): ";
+  for (auto elem : std_list3) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+
+  std::cout << "LinkedList 1: ";
+  for (auto elem : linked_list1) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+
+  std::cout << "LinkedList 2 (copy): ";
+  for (auto elem : linked_list2) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+
+  std::cout << "LinkedList 3 (assignment): ";
+  for (auto elem : linked_list3) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+
+  // Check that all lists are equal
+  assert(compare_lists(std_list1, linked_list1));
+  assert(compare_lists(std_list2, linked_list2));
+  assert(compare_lists(std_list3, linked_list3));
+}
+
+// Test function for LinkedList with custom allocator
+void test_linked_list_with_custom_allocator() {
+  LinkedList<int, StackAllocator<int, 100>> list;
+  list.push_front(1);
+  list.push_front(2);
+  list.push_front(3);
+  list.push_back(4);
+
+  std::cout << "Linked list with custom allocator: ";
+  for (auto elem : list) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+
+  list.pop_front();
+  std::cout << "Linked list after popping: ";
+  for (auto elem : list) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+}
+
 int main() {
   std::cout << "Testing StackAllocator..." << '\n';
   test_stack_allocator();
@@ -102,4 +373,14 @@ int main() {
   std::cout << "Benchmarking LinkedList with StackAllocator:" << '\n';
   LinkedList<int, StackAllocator<int, 100'010>> linked_list_cus_alloc;
   benchmark_list(linked_list_cus_alloc, num_elements);
+
+  test_copy_constructor_and_assignment_operator();
+  test_compare_with_std_forward_list();
+
+  std::cout << "Testing LinkedList with std::forward_list...\n";
+  test_copy_constructor_and_assignment();
+
+  std::cout << "Testing LinkedList with custom allocator...\n";
+  test_linked_list_with_custom_allocator();
+
 }
